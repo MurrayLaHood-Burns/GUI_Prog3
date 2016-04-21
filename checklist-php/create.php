9083f36e556a643e8b4edac678857539e14d8f6d
unknown <1640636@T-SMD1020394.SDSMT.local>
2016-04-21 16:25:50 -0600

    modified course-checklist.xml to have tag attributes instead of element ids. Also started on create.php

--- checklist-php/create.php
+++ checklist-php/create.php
@@ -1,6 +1,35 @@
 <?php
 
-
+if(isset ($_POST['update']))
+{
+    $xml = new DomDocument("1.0", "UTF-8");
+    
+    $xml.simplexml_load_file('..\\student-checklists\\course-checklist.xml');
+    
+    $checklist = $xml->getElementsByTagName("checklist");
+    
+    $freshman = $checklist->standing->getElementById("fresh");
+    
+    $sophmore = $checklist->standing->getElementById("soph");
+    
+    $junior = $checklist->standing->getElementById("junior");
+    
+    $senior = $checklist->standing->getElementById("senior");
+    
+    $cscElect = $checklist->csc_elective;
+    
+    $mathElect = $checklist->math_elective;
+    
+    $sciRequ = $checklist->science_requirement;
+    
+    $libArt = $checklist->libarts;
+    
+    $freeCourse = $checklist->free;
+    
+    
+    
+    
+}
 
 
 ?>
